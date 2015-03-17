e7c98d8a98555efe3bd73bad6211d448d661db47
Paolo Boschini <boschini.paolo@gmail.com>
2015-03-17 11:04:05 +0100

    Initial Commit

---
+++ HelloGalaxy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloGalaxy
+//
+//  Created by Paolo Boschini on 17/03/15.
+//  Copyright (c) 2015 Paolo Boschini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
